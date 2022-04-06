ec54e6950cab00bd88a93be483fa405ecc9b3d02
Juan Mosquella <jmosquella11@gmail.com>
2022-04-06 19:33:46 -0300

    Terminados los estilos genrales de la web

--- js/faq.js
+++ js/faq.js
@@ -1,15 +1,17 @@
 export default function faq(){
 
     const question = document.querySelectorAll('.container .container-question');
+
+    question.forEach(el => {
+        el.addEventListener('click', ()=>{
+            el.classList.toggle('is-active');        
+        })                
+    });
             
 
-    document.addEventListener('click', () => {
-        question.forEach(el => {
-            el.addEventListener('click', ()=>{
-                el.classList.toggle('is-active');        
-            })                
-        });
+    
+        
 
         
-    })  
+      
 }
